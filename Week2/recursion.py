e56f7164bbc2304318aee51806962750f0e53a9d
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2021-12-13 20:45:25 -0500

    Add files via upload

--- Week2/recursion.py
+++ Week2/recursion.py
@@ -19,10 +19,33 @@ def loop2():
 
 def loop1Rec(num,odd_sum):
     # Duplicate the loop1 function using recursion
+    # Check for stop condition
+    if (num > 20):
+        return odd_sum
+    else:
+        if num % 2 == 1:
+            odd_sum += num
+        # Re-call the function with updating the number with +1
+        return loop1Rec(num + 1, odd_sum)
 
     
 
 def loop2Rec(num,even_sum):
     # Duplicate the loop2 function using recursion
+    if num < 0:
+        return even_sum
+    else:
+        if num % 2 == 0:
+            even_sum += num
+        return loop2Rec(num - 1, even_sum)
 
+loop1_res = loop1()
+print("Sum of odds between 1 and 20 using 'for' loop =  {}".format(loop1_res))
+loop1rec_res = loop1Rec(0, 0)
+print("Sum of odds between 1 and 20 using recursion =  {}".format(loop1rec_res))
+
+loop2_res = loop2()
+print("Sum of evens between 1 and 20 using 'for' loop =  {}".format(loop2_res))
+loop2rec_res = loop2Rec(19, 0)
+print("Sum of evens between 1 and 20 using recursion =  {}".format(loop2rec_res))
 
