42056aae8496f12077c1bca7f842a316fe13f2be
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2022-01-09 23:59:39 -0500

    Add files via upload

---
+++ Week6/shuffle.py
@@ -0,0 +1,27 @@
+import random
+
+def shuffle(lst):
+    for i in range(len(lst)):
+        new_i = random.randint(0, i)
+        if i != new_i:
+            lst[i], lst[new_i] = lst[new_i], lst[i]
+    return lst
+
+
+lst = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
+
+print("Before Shuffle:")
+print(lst)
+
+print("Shuffled:")
+
+lst = shuffle(lst)
+print(lst)
+lst = shuffle(lst)
+print(lst)
+lst = shuffle(lst)
+print(lst)
+lst = shuffle(lst)
+print(lst)
+lst = shuffle(lst)
+print(lst)
